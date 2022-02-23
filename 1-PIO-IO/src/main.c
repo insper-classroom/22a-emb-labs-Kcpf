aa68738f3a2b87774cda6904ccb38b024b80195c
Kcpf <fernandopmgf@al.insper.edu.br>
2022-02-22 23:04:03 -0800

    adicionando entrega final LAB1

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -31,6 +31,11 @@
 /* defines                                                              */
 /************************************************************************/
 
+#define LED_PIO PIOC
+#define LED_PIO_ID ID_PIOC
+#define LED_PIO_IDX 8
+#define LED_PIO_IDX_MASK (1u << LED_PIO_IDX)
+
 #define LED1_PIO PIOA           
 #define LED1_PIO_ID ID_PIOA    
 #define LED1_PIO_IDX 0       
@@ -47,6 +52,11 @@
 #define LED3_PIO_IDX_MASK (1u << LED3_PIO_IDX)
 
 // Configuracoes do botao
+#define BUT_PIO PIOA
+#define BUT_PIO_ID ID_PIOA
+#define BUT_PIO_IDX 11
+#define BUT_PIO_IDX_MASK (1 << BUT_PIO_IDX)
+
 #define BUT1_PIO PIOD
 #define BUT1_PIO_ID ID_PIOD
 #define BUT1_PIO_IDX 28
@@ -95,24 +105,29 @@ void init(void)
 	WDT->WDT_MR = WDT_MR_WDDIS;
 	
 	// Ativa o PIO na qual os LEDs foram conectados
+	pmc_enable_periph_clk(LED_PIO_ID);
 	pmc_enable_periph_clk(LED1_PIO_ID);
 	pmc_enable_periph_clk(LED2_PIO_ID);
 	pmc_enable_periph_clk(LED3_PIO_ID);
 	
 	// Inicializa PIOs dos botoes
+	pmc_enable_periph_clk(BUT_PIO_ID);
 	pmc_enable_periph_clk(BUT1_PIO_ID);
 	pmc_enable_periph_clk(BUT2_PIO_ID);
 	pmc_enable_periph_clk(BUT3_PIO_ID);
 
 	//Inicializa PC8 como saída
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
 	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
 	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
 	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
 	
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
 	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
 	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
 	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
 	
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
 	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
 	pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
 	pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
@@ -133,14 +148,28 @@ int main(void)
   // aplicacoes embarcadas não devem sair do while(1).
   while (1)
   {
+	  
+	if(!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
+		for (int c = 0; c < 5; c++) {
+			// coloca 0 no pino do LED
+			pio_clear(LED_PIO, LED_PIO_IDX_MASK);
+			delay_ms(500);
+			  	
+			pio_set(LED_PIO, LED_PIO_IDX_MASK);
+			delay_ms(500);
+		}
+	} else {
+		pio_set(LED_PIO, LED_PIO_IDX_MASK);
+	}
+	
 	if(!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)) {
 		for (int c = 0; c < 5; c++) {
 		// coloca 0 no pino do LED
 		pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
-		delay_ms(200);
+		delay_ms(500);
 			
 		pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
-		delay_ms(200);
+		delay_ms(500);
 		}
 	} else {
 		pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
@@ -150,12 +179,12 @@ int main(void)
 		for (int c = 0; c < 5; c++) {
 			// coloca 0 no pino do LED
 			pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
-			delay_ms(200);
+			delay_ms(500);
 			  	  
 			pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
-			delay_ms(200);
+			delay_ms(500);
 		}
-		} else {
+	} else {
 		pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
 	}
 	
@@ -163,12 +192,12 @@ int main(void)
 		for (int c = 0; c < 5; c++) {
 			// coloca 0 no pino do LED
 			pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
-			delay_ms(200);
+			delay_ms(500);
 				
 			pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
-			delay_ms(200);
+			delay_ms(500);
 		}
-		} else {
+	} else {
 		pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
 	}
 			
